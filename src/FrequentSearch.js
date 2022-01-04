2fad47003d2bfc7360f876eecd02866deeb29720
Junior Claudio Zavaleta <shuni95@protonmail.com>
2022-01-03 22:11:06 -0500

    Contional rendering with components in functions and update styles

--- src/FrequentSearch.js
+++ src/FrequentSearch.js
@@ -1,5 +1,38 @@
 import { Component } from "react";
 
+function FrequentSearchOn(props) {
+    return (
+        <div className="mx-2 px-2 mt-2">
+            <div className="">
+                <span className="text-gray-700 text-xl font-bold">
+                    &lt;
+                </span>
+                <span className="ml-2 text-emerald-300 text-xl font-medium">
+                    <button onClick={props.onClick} className="py-2 px-4 rounded-lg border-2 border-white bg-black">{props.title}</button>                
+                </span> 
+            </div>
+            <div className="mt-2">
+                <p className="bg-gray-100 border-solid border-2 border-indigo-200 p-4">Hidden content</p>
+            </div>
+        </div>      
+    )
+}
+
+function FrequentSearchOff(props) {
+    return (
+        <div className="mx-2 px-2 mt-2">
+            <div className="">
+                <span className="text-gray-300 text-xl font-bold">
+                    &gt;
+                </span>
+                <span className="ml-2 text-black text-xl font-medium">
+                    <button onClick={props.onClick} className="py-2 px-4 rounded-lg border-2 border-emerald-700">{props.title}</button>                
+                </span> 
+            </div>
+            <div className="mt-2"></div>    
+        </div>
+    )
+}
 
 export class FrequentSearch extends Component {
 
@@ -15,22 +48,10 @@ export class FrequentSearch extends Component {
     }
 
     render() {
-        const className = this.state.contentVisible ? '' : 'hidden';
-
-        return (
-            <div className="mx-2 px-2 mt-2">
-                <div className="">
-                    <span className="text-indigo-800 text-xl font-bold">
-                        {this.state.contentVisible ? '<' : '>'}
-                    </span>
-                    <span className="ml-2 text-emerald-700 text-xl font-medium">
-                        <a href="#" onClick={this.toggle}>{this.props.title}</a>
-                    </span> 
-                </div>
-                <div className={className + " mt-2"}>
-                    <p className="bg-gray-100 border-solid border-2 border-indigo-200 p-4">Hidden content</p>
-                </div>            
-            </div>
-        )
+        if (this.state.contentVisible) {
+            return <FrequentSearchOn onClick={this.toggle} title={this.props.title}/>;
+        } else {
+            return <FrequentSearchOff onClick={this.toggle} title={this.props.title}/>;
+        }
     }
 }
