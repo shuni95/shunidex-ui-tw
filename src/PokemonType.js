2979d35ace70040cb439ef65d43dbae0d47ac409
Junior Claudio Zavaleta <shuni95@protonmail.com>
2022-02-18 21:59:44 -0500

    Fix pokemon types color didn't load - Add .json to tailwind.config.js

--- src/PokemonType.js
+++ src/PokemonType.js
@@ -4,9 +4,7 @@ function PokemonType(props) {
   const pokemonType = PokemonTypeMap[props.type];
 
   return (
-    <span
-      className={pokemonType.classes + " p-1 px-3 rounded-3xl leading-6 lowercase text-sm font-['Open_Sans'] italic"}
-    >
+    <span className={pokemonType.classes + " p-1 px-3 rounded-3xl leading-6 lowercase text-sm font-['Open_Sans']"}>
       {pokemonType.text}
     </span>
   );
